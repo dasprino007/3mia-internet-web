901cbed1c4c715f632bf4e1c1f8f212e1683e3ef
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-12 10:54:57 -0300

    style do overflow-x da pagina filmes feita

--- deguflix/src/pages/Filmes.jsx
+++ deguflix/src/pages/Filmes.jsx
@@ -1,3 +1,4 @@
+import { data } from "autoprefixer";
 import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from 'react-router-dom'
@@ -18,41 +19,27 @@ function Filmes() {
         else if(data > 4){
             return('bg-MediumRate-color')
         }
-        else{
-            return('bg-BadRate-color')
-        }
+        else
+        return('bg-BadRate-color')
     }
 
     return ( 
-        <>  <h2 className="mb-5">Filmes populares</h2>
+        <>  
+            <h2 className="mb-5">Filmes populares</h2>
             <div className="listasFilmes flex overflow-x-auto gap-5">
                 {
                     filmes.map(
                         filme => (
                             <>
                             <div className="card-filmes relative group aspect-poster mobile:h-60 sm:min-h-72 md:min-h-80" key={filme.id}>
+                            <Link to={`${filme.id}`}><img className="group-hover:opacity-30" src={`https://image.tmdb.org/t/p/w500/${filme.poster_path}`} alt="" /><img/></Link>
                             <div className="invisible group-hover:visible absolute bottom-3 left-1 center">
                                 <h1 className="text-wrap">{filme.title}</h1>
-                                <h2 className="">Lançamento: {filme.release_date}</h2>
+                                <h2 className="text-wrap">Lançamento: {filme.release_date}</h2>
                             </div>
-                            <div className={`size-8 flex items-center justify-center invisible group-hover:visible absolute top-1 right-1 hover:opacity-0 ${ClassRate(filme.vote_average)}`}>
+                            <div className={`size-8 ${ClassRate(filme.vote_average)} rounded-md border-2 border-white flex items-center justify-center invisible group-hover:visible absolute top-1 right-1`}>
                             <h2>{Math.round((filme.vote_average)*10)}</h2>
                             </div>
-                            <Link to={`${filme.id}`}><img className="group-hover:opacity-30" src={`https://image.tmdb.org/t/p/w500/${filme.poster_path}`} alt="" /><img/></Link>
-                            </div>
-                            </>
-                        )
-                    )
-                }
-            </div>
-            <div className="listasFilmes flex flex-row flex-wrap">
-                {
-                    filmes.map(
-                        filme => (
-                            <>
-                            <div className="card-filmes" key={filme.id}>
-                                <img src={`https://image.tmdb.org/t/p/w500/${filme.poster_path}`} alt="" />
-                                <h1 className="text-wrap">{filme.title}</h1>
                             </div>
                             </>
                         )

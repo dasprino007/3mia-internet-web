a0aba1416352b225f3f1daa70363c100d5668954
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-14 15:20:59 -0300

    it just works

--- deguflix/src/pages/Contato.jsx
+++ deguflix/src/pages/Contato.jsx
@@ -1,19 +1,29 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
+import emailjs from '@emailjs/browser'
 
 function Contato() {
-    const form = useRef()
     const [email, setemail] = useState('')
     const [name, setname] = useState('')
     const [sobrenome, setsobrenome] = useState('')
     const [dogname, setdogname] = useState('')
     const [desc, setdesc] = useState('')
     
-    const sendEmail = (e) => {
+    const Email = (e) => {
         if(email === '' || name === '' || sobrenome === '' || dogname === '' || desc === ''){
             alert("preencha os campos")
             return;
         }
         e.preventDefault();
+
+        const contatoTemplate = {
+            email: email,
+            to_name : name,
+            to_sobrenome: sobrenome,
+            to_dogname : dogname
+        }
+
+        emailjs.send('service_9bor2dc','template_rjf7n49',contatoTemplate,'smLEttR_Bvrf_1o6j')
+        .then(response => console.log(response, "\n email enviado"))
     }
 
     return (
@@ -23,14 +33,14 @@ function Contato() {
         <div className="md:w-1/2">
         <p>A gente não vai responder esse email e so estamos colocando isso porque nosso chefe mando ter então toma ea depois não reclame se sua senha foi roubada.</p>
 
-            <form className="flex flex-col mb-5 gap-3" ref={form} onSubmit={sendEmail}>
+            <form className="flex flex-col mb-5 gap-3" onSubmit={Email}>
             <div className="flex flex-col">
                     <label className="font-semibold" htmlFor="email">Email:</label>
-                    <input className=" bg-secondary-color rounded-md h-8 font-semibold text-md" type="text" name="email" id="email" required value={email} onChange={(e) => setname(e.target.value)}/>
+                    <input className=" bg-secondary-color rounded-md h-8 font-semibold text-md" type="text" name="email" id="email" required value={email} onChange={(e) => setemail(e.target.value)}/>
                 </div>
                 <div className="flex flex-col">
                     <label className="font-semibold" htmlFor="nome">Nome:</label>
-                    <input className=" bg-secondary-color rounded-md h-8 font-semibold text-md" type="text" name="nome" id="nome" required value={name} onChange={(e) => setemail(e.target.value)}/>
+                    <input className=" bg-secondary-color rounded-md h-8 font-semibold text-md" type="text" name="nome" id="nome" required value={name} onChange={(e) => setname(e.target.value)}/>
                 </div>
                 <div className="flex flex-col">
                     <label className="font-semibold" htmlFor="sobrenome">Sobrenome:</label>
